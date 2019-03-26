42941e2f1a6315d64c79957f91b0560006fd2b68
maltyncev <maltyncev@gmail.com>
2019-03-26 12:30:09 +0500

    Initial commit

---
+++ app/src/main/java/ru/startandroid/p0401_layoutinflater/MainActivity.java
@@ -0,0 +1,42 @@
+package ru.startandroid.p0401_layoutinflater;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    /**
+     * Called when the activity is first created.
+     */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LayoutInflater ltInflater = getLayoutInflater();
+
+        LinearLayout linLayout = (LinearLayout) findViewById(R.id.linLayout);
+        View view1 = ltInflater.inflate(R.layout.activity_text, linLayout, true);
+        LayoutParams lp1 = view1.getLayoutParams();
+
+        Log.d(LOG_TAG, "Class of view1: " + view1.getClass().toString());
+        Log.d(LOG_TAG, "Class of layoutParams of view1: " + lp1.getClass().toString());
+        //Log.d(LOG_TAG, "Text of view1: " + ((TextView) view1).getText());
+
+        RelativeLayout relLayout = (RelativeLayout) findViewById(R.id.relLayout);
+        View view2 = ltInflater.inflate(R.layout.activity_text, relLayout, true);
+        LayoutParams lp2 = view2.getLayoutParams();
+
+        Log.d(LOG_TAG, "Class of view2: " + view2.getClass().toString());
+        Log.d(LOG_TAG, "Class of layoutParams of view2: " + lp2.getClass().toString());
+        //Log.d(LOG_TAG, "Text of view2: " + ((TextView) view2).getText());
+    }
+}
